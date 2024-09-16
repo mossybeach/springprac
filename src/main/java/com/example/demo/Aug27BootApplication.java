13fb7a4429256ff2731bd4260f1738031fbfcd80
mossybeach <lyyoon825@gmail.com>
2024-09-17 00:01:30 +0900

    initial commit

---
+++ src/main/java/com/example/demo/Aug27BootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aug27BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aug27BootApplication.class, args);
+	}
+
+}
